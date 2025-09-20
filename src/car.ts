c2d5d4b4f456d5182acac45b26402eadea5023b2
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2025-09-20 14:33:14 +0200

    Detect collisions with borders - Lesson 4

--- src/car.ts
+++ src/car.ts
@@ -1,6 +1,7 @@
 import Controls from './controls';
 import Sensor from './sensor';
-import type { Borders } from './types';
+import type { Borders, Point } from './types';
+import { polygonsIntersect } from './utils/polygonsIntersect';
 
 class Car {
   controls: Controls;
@@ -10,6 +11,8 @@ class Car {
   friction: number;
   angle: number;
   sensor: Sensor;
+  polygon: Point[];
+  damaged = false;
 
   constructor(
     public x: number,
@@ -24,22 +27,57 @@ class Car {
     this.angle = 0;
     this.sensor = new Sensor(this);
     this.controls = new Controls();
+    this.polygon = this.createPolygon();
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
-    ctx.fillStyle = 'red';
-    ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+    if (this.damaged) {
+      ctx.fillStyle = 'gray';
+    } else {
+      ctx.fillStyle = 'blue';
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
     ctx.fill();
-    ctx.restore();
     this.sensor.draw(ctx);
   }
 
   update(roadBorders: Borders) {
-    this.move();
+    if (!this.damaged) {
+      this.move();
+      this.polygon = this.createPolygon();
+      this.damaged = this.assessDamage(roadBorders);
+    }
     this.sensor.update(roadBorders);
+  }
+
+  private createPolygon() {
+    const points: Point[] = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const angle = Math.atan2(this.width, this.height);
+    // top right, top left, bottom left, bottom right
+    points.push({
+      x: this.x - Math.sin(this.angle - angle) * rad,
+      y: this.y - Math.cos(this.angle - angle) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(this.angle + angle) * rad,
+      y: this.y - Math.cos(this.angle + angle) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - angle) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - angle) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + angle) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + angle) * rad,
+    });
+
+    return points;
   }
 
   private move() {
@@ -75,6 +113,16 @@ class Car {
         this.angle -= 0.03 * flip;
       }
     }
+  }
+
+  assessDamage(roadBorders: Borders) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      const border = roadBorders[i];
+      if (polygonsIntersect(this.polygon, border)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
---
+++ src/utils/polygonsIntersect.ts
@@ -0,0 +1,19 @@
+import type { Point } from '../types';
+import { getIntersection } from './getIntersection';
+
+export const polygonsIntersect = (poly1: Point[], poly2: Point[]) => {
+  for (let i = 0; i < poly1.length; i++) {
+    for (let j = 0; j < poly2.length; j++) {
+      const touch = getIntersection(
+        poly1[i],
+        poly1[(i + 1) % poly1.length],
+        poly2[j],
+        poly2[(j + 1) % poly2.length]
+      );
+      if (touch) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
